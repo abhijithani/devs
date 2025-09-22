db73b15ba9e1617bc9dc82b2ef2c9f5ecd131ca0
abhijithani <abhijithmscr7@gmail.com>
2025-09-22 17:25:50 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
 const express = require("express");
 
 const app = express();
-const { adminAuth,useAuth } = require("./middlwares/auth")
+const { adminAuth, useAuth } = require("./middlwares/auth")
 
-//Handle Auth Middlewares for all GET, POST...
-app.use("/admin", adminAuth);
 
-app.get("/admin/getAllData", (req, res, next) => {
-    res.send("all data sent");
+app.get("/getUserData", (req, res, next) => {
+    throw new Error("fdahkjh")
+    // try{
+    //     res.send("all data sent");
+    // }
+    // catch(err) {
+    //     res.status(500).send("something went wrong contact support")
+    // }
 });
 
-app.get("/admin/deleteUser", (req, res, next) => {
-    res.send("delete a user");
-});
- 
-app.use("/user/data",useAuth, (req, res,) =>{
-    res.send("data is sent");
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("something went wrong");
+    }
+    else {
+        res.send("hii")
+    }
 })
-
-app.use("/user/login", (req, res,) =>{
-    res.send("user logged in successfully");
-})
-
 
 
 app.listen(3000, () => {
     console.log("server is lisetening in port 3000");
-    
+
 }); 
