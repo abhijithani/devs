7715046fac08766d1115acd4138a2369ba558d01
abhijithani <abhijithmscr7@gmail.com>
2025-09-25 19:00:45 +0530

    user auth middleware

--- src/app.js
+++ src/app.js
@@ -2,67 +2,93 @@ const express = require("express");
 const connectDB = require("./config/dataBase");
 const app = express();
 const User = require("./models/user")
-const {validateSignUpdata} = require("./utils/validation")
+const { validateSignUpdata } = require("./utils/validation")
 const bcrypt = require("bcrypt")
-
-
+const cookieParse = require("cookie-parser")
+const jwt = require("jsonwebtoken");
 app.use(express.json());
+app.use(cookieParse());
+const {userAuth} = require("./middlwares/auth")
 
-app.post("/signUp" ,async (req, res) => {
+
+app.post("/signUp", async (req, res) => {
     try {
-    //Validation of data
-    validateSignUpdata(req);
-    
-    const {firstName, lastName, emailId, password} = req.body;
+        //Validation of data
+        validateSignUpdata(req);
 
-    //Encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
+        const { firstName, lastName, emailId, password } = req.body;
+
+        //Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
         console.log(passwordHash);
-        
 
-    //Creating a new instance fo the user model
-     const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash,
-     });
-         await user.save();
-         res.send("user added sucessfully");
-     }
-     catch (err){
+
+        //Creating a new instance fo the user model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
+        await user.save();
+        res.send("user added sucessfully");
+    }
+    catch (err) {
         res.status(400).send("ERROR: " + err.message)
-     }
+    }
 });
 
-app.post("/login",async (req, res) => {
-    try{
-        const {emailId, password} = req.body;
-        const user = await User.findOne({emailId: emailId});
-        if(!user){
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
             throw new Error("user is not present")
         }
         const isPasswordValid = await bcrypt.compare(password, user.password)
-        
-        if(isPasswordValid){
+
+        if (isPasswordValid) {
+            //Create a JWT Token
+
+            const token = await jwt.sign({ _id: user._id }, "DEVS@2025", {expiresIn: "1d"});
+            console.log(token);
+
+
+            res.cookie("token", token);
             res.send("login succesfully")
         }
         else {
             throw new Error("password Not correct")
         }
     }
-    catch(err) {
+    catch (err) {
         res.status(400).send("ERROR: " + err.message)
     }
+})
+
+app.get("/profile",userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        res.send(user)
+    }
+    catch (err) {
+        res.status(400).send("ERROR: " + err.message)
+    }
+})
+
+app.post("/sendConnectionRequest", userAuth, async(req, res) => {
+    const user = req.user;
+    console.log("sending a connectin request");
+    res.send(user.firstName+ "sent the connection request")
 })
 
 //get user by email
 app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
     // console.log(userEmail);
-    
+
     try {
-        const user = await User.findOne({emailId : userEmail})
+        const user = await User.findOne({ emailId: userEmail })
         res.send(user);
         // const user = await User.find({emailId : userEmail});
         // if(user.length === 0) {
@@ -72,12 +98,12 @@ app.get("/user", async (req, res) => {
         //     res.send(user)
         // }
     }
-    catch (err){
+    catch (err) {
         res.status(400).send("something went wrong")
     }
 })
 
-app.get("/feed",async (req, res) => {
+app.get("/feed", async (req, res) => {
     try {
         const users = await User.find({});
         res.send(users)
@@ -87,7 +113,7 @@ app.get("/feed",async (req, res) => {
     }
 })
 
-app.delete("/user",async (req, res) => {
+app.delete("/user", async (req, res) => {
     const userId = req.body.userId;
     try {
         const userDelete = await User.findByIdAndDelete(userId);
@@ -103,7 +129,7 @@ app.patch("/user/:userId", async (req, res) => {
     const UserId = req.params?.userId;
     const data = req.body;
 
-    
+
     try {
         const ALLOWED_UPDATES = [
             "photoUrl", "about", "userId", "skills", "gender", "age"
@@ -114,18 +140,18 @@ app.patch("/user/:userId", async (req, res) => {
             throw new Error("update not allowed");
         }
 
-        if(data?.skills.length > 10){
+        if (data?.skills.length > 10) {
             throw new Error("update not allowed")
         }
 
-        const user = await User.findByIdAndUpdate({ _id: UserId }, data,{
+        const user = await User.findByIdAndUpdate({ _id: UserId }, data, {
             returnDocument: "after",
             runValidators: true,
         })
         res.send("user updated successufully")
     }
     catch (err) {
-        res.status(400).send("something went wrong"+ err.message)
+        res.status(400).send("something went wrong" + err.message)
     }
 })
 
@@ -133,9 +159,8 @@ connectDB().then(() => {
     console.log("Database connection is established")
     app.listen(3000, () => {
         console.log("server is lisetening in port 3000");
-    }); 
+    });
 })
     .catch((err) => {
         console.error("Database cannot be connected!");
-})
- 
+    })
--- src/middlwares/auth.js
+++ src/middlwares/auth.js
@@ -1,30 +1,30 @@
-const adminAuth = (req, res, next) => {
-    console.log("Admin auth is getting checked!");
-    const token = "xyz";
-    const authentication = token === "xyz"
-    if (!authentication) {
-        res.status(401).send("Unathourised request");
-    }
-    else {
-        next();
-    }
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
 
+//user auth middleware
+const userAuth = async (req, res, next) => {
+    
+    try{
+    const {token} = req.cookies;
+    if(!token){
+        throw new Error ("Token is not valid!!");
+    }
+    const decodedObj = await jwt.verify(token, "DEVS@2025");
+
+    const {_id} = decodedObj;
+
+    const user = await User.findById(_id);
+    if(!user){
+        throw new Error("user Not found")
+    }
+    req.user = user; 
+    next();
 }
-
-const useAuth = (req, res, next) => {
-    console.log("Admin auth is getting checked!");
-    const token = "xyz";
-    const authentication = token === "xyz"
-    if (!authentication) {
-        res.status(401).send("Unathourised request");
-    }
-    else {
-        next();
-    }
-
+catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+}
 }
 
 module.exports = {
-    adminAuth,
-    useAuth,
+    userAuth,
 }
